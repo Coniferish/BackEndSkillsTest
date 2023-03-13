a0190cc2c6f7bfada9eb50a6166452c8336aad71
Coniferish <johnjennings702@gmail.com>
2023-03-13 14:40:48 -0500

    feat: Init endpoint for Q2

--- backEndSkillsTest/__init__.py
+++ backEndSkillsTest/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 # from .db import get_states_in_region, get_migration_to_region_in_year
 from .db import *
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -18,6 +19,13 @@ def migration_to_region_in_year(region, year):
 def num_of_states_min_10k_moved(state):
     response = jsonify(get_num_of_states_min_10k_moved(state))
     return response
+
+@app.route('/q2/<state>')
+def q2(state):
+    count_10k = get_num_of_states_min_10k_moved(state)
+    count_df = pd.DataFrame(count_10k, columns=['Count', 'Year'])
+    print(count_df)
+    return count_10k
 
 if __name__ == '__main__':
     app.run(debug=True)
