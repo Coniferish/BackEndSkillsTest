9ee4b71ccaea11e4e964e4ec5bd4c9c85eae4b7a
Coniferish <johnjennings702@gmail.com>
2023-04-05 17:30:26 -0500

    feat: Run/use Postgres server

--- backEndSkillsTest/db.py
+++ backEndSkillsTest/db.py
@@ -1,12 +1,39 @@
-import mysql.connector
+import psycopg2
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-sql_password = os.environ['MYSQL_PASSWORD']
+psql_password = os.environ['POSTGRES_PASSWORD']
 database = 'state_migrations'
+
+def create_connection(host_name, user_name, user_password, db_name=None):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print('MySQL Database connection successful')
+    except Exception as err:
+        print(f"Error: '{err}'")
+    return connection
+
+def get_query(query):
+    connection = create_connection('localhost', 'postgres', psql_password, database)
+    cursor = connection.cursor()
+    try:
+        result = []
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        return result
+    except Exception as err:
+        print(f"Error: '{err}'")
 
 q_all_divisions = """
     SELECT DISTINCT s.parent_id
@@ -140,33 +167,7 @@ q_all_migrations_to_state = """
     WHERE m.current_state = '{state}'
     GROUP BY year;
     """
-
-def create_connection(host_name, user_name, user_password, db_name=None):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print('MySQL Database connection successful')
-    except Exception as err:
-        print(f"Error: '{err}'")
-    return connection
-
-def get_query(query):
-    connection = create_connection('localhost', 'root', sql_password, database)
-    cursor = connection.cursor()
-    try:
-        result = []
-        cursor.execute(query)
-        for row in cursor:
-            result.append(row)
-        cursor.close()
-        return result
-    except Exception as err:
-        print(f"Error: '{err}'")
+    
         
 def get_all_divisions():
     return get_query(q_all_divisions)
